a9b73a31fa86897cf5f0d6163b87f28968472c09
michael_fanning <166875436+michael-fanning@users.noreply.github.com>
2026-01-09 09:17:50 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function loadHeader() {
+  fetch('header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header-placeholder').innerHTML = data);
+}
+loadHeader();
