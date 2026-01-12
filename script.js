48b94bc4b91a130cd39a7b5566dc6accd60c713f
michael_fanning <166875436+michael-fanning@users.noreply.github.com>
2026-01-12 12:31:10 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ function loadHeader() {
     .then(data => document.getElementById('header-placeholder').innerHTML = data);
 }
 loadHeader();
+
+//Carousel
+let counter = 0;
+
+function initCarousel() {
+    const slide = document.querySelector('.carousel-slide');
+    const images = document.querySelectorAll('.carousel-slide img');
+    const nextBtn = document.querySelector('#nextBtn');
+    const prevBtn = document.querySelector('#prevBtn');
+
+    if(!slide || images.length === 0) return;
+
+    nextBtn.addEventListener('click', () => {
+        counter++;
+        if (counter >= images.length) counter = 0;
+        slide.style.transform = `translateX(${-100 * counter}%)`;
+    });
+
+    prevBtn.addEventListener('click', () => {
+        counter--;
+        if (counter < 0) counter = images.length - 1;
+        slide.style.transform = `translateX(${-100 * counter}%)`;
+    });
+
+    setInterval(() => {
+        nextBtn.click();
+    }, 5000);
+}
+
+window.addEventListener('DOMContentLoaded', initCarousel);
